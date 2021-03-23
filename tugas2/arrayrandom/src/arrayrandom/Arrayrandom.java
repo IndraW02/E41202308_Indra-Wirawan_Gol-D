59aeec3a3927658b9e8c7273c6023ae62e6fc32a
IndraW02 <iwirawan54@gmail.com>
2021-03-23 08:31:26 +0700

    5b tugas 2

---
+++ tugas2/arrayrandom/src/arrayrandom/Arrayrandom.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayrandom;
+import java.util.Random;
+/**
+ *
+ * @author Indra Wirawan
+ */
+public class Arrayrandom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random angkaRandom = new Random();
+        int hasil;
+            
+           for ( int counter = 1; counter <= 200; counter++) {
+               hasil = 1 + angkaRandom.nextInt(9);
+               System.out.printf( "%d ", hasil );
+               
+               if ( counter % 20 == 0 )
+                System.out.println();
+           }
+    }
+    
+}
