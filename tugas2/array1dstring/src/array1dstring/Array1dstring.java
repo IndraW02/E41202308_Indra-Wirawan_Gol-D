2edce0416e7711295bde5b665a0a388f2e67f286
IndraW02 <iwirawan54@gmail.com>
2021-03-23 08:23:02 +0700

    5a tugas 2

---
+++ tugas2/array1dstring/src/array1dstring/Array1dstring.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array1dstring;
+
+/**
+ *
+ * @author Indra Wirawan
+ */
+public class Array1dstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"indra","wirawan"};
+        
+        for(int j=0; j<teks.length; j++){
+            System.out.print(teks[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
