059a78243d409f02ba8816c76c996bcb14d878de
IndraW02 <iwirawan54@gmail.com>
2021-03-23 16:12:46 +0700

    5c tugas 2

--- tugas2/arrray2d/src/arrray2d/Arrray2d.java
+++ tugas2/arrray2d/src/arrray2d/Arrray2d.java
@@ -17,10 +17,10 @@ public class Arrray2d {
     public static void main(String[] args) {
         // TODO code application logic here
         String[][]huruf = {
-            {"A","B","C","D"},
-            {"E","F","G","H"},
-            {"I","J","K","L"},   
-            {"M","N","o","P"},
+            {"1","2","3","4"},
+            {"5","6","7","8"},
+            {"9","10","11","12"},   
+            {"13","14","15","16"},
         };
         
          for (int i=0;i<4;i++){
