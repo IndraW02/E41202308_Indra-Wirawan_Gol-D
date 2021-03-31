7fe656b379c57d41c239af01c1345bbfef1a2f66
IndraW02 <iwirawan54@gmail.com>
2021-03-31 19:28:41 +0700

    Update Pengurutangenap.java

--- tugas3/pengurutangenap/src/pengurutangenap/Pengurutangenap.java
+++ tugas3/pengurutangenap/src/pengurutangenap/Pengurutangenap.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package pengurutangenap;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -17,20 +18,30 @@ public class Pengurutangenap {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-           Scanner input = new Scanner(System.in);
-        System.out.println("Masukkan Jumlah Angka :");
-        int jmlAngka = input.nextInt();
-        int[]arr = new int[jmlAngka];
-        System.out.println("Masukkan Semua Angka :");
- 
-        for(int i=0;i <jmlAngka;i++){
-         arr[i] =input.nextInt();
-     }
- 
-     Arrays.sort(arr);
-     for(int index:arr){
-         System.out.println("Telah diurutkan :"+index);
-     }
+         Scanner s = new Scanner(System.in);
+        int panjang = 0;
+                System.out.print("masukan panjang nilai : ");
+            panjang = s.nextInt();
+        int[] angka = new int[panjang];
+
+        for (int i = 0; i < panjang; i++) {
+                System.out.print("masukan nilai ke-" + i + " : ");
+            angka[i] = s.nextInt();
+        }
+
+                System.out.print("[ ");
+        for (int i : angka) {
+                System.out.print(i + " ");
+        }
+              System.out.println("]");
+
+        Arrays.sort(angka);
+                System.out.println("Nilai setelah diurutkan dari yang terkecil - terbesar");
+                System.out.println("=====================================================");
+            System.out.print("[ ");
+        for (int i : angka) {
+            System.out.print(i + " ");
+        }
+        System.out.print("]");
     }
-    
 }
